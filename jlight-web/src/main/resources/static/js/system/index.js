a1eb55f790487547771e47f0905cc037889a9ef6
GoQeng <857963937@qq.com>
2016-11-27 20:40:32 +0800

    clean js code

--- jlight-web/src/main/resources/static/js/system/index.js
+++ jlight-web/src/main/resources/static/js/system/index.js
@@ -1,41 +1,34 @@
- jQuery(document).ready(function(){
-	//初始化宽度、高度
-    $("#main-container").height($(window).height()-76);
-	$("#iframe").height($(window).height()-140);
-	$(".sidebar").height($(window).height()-99);
-	 var thisHeight = $("#nav_list").height($(window).height()-185);
-//	 $(".submenu").height($(thisHeight).height()-160);
-	 $("#nav_list").children(".submenu").css("height",thisHeight);
+$(function () {
+    var windowHeight = $(window).height();
+    $("#main-container").height(windowHeight - 76);
+    $("#iframe").height(windowHeight - 140);
+    $(".sidebar").height(windowHeight - 99);
+    var navHeight = $("#nav_list").height(windowHeight - 185);
+    $("#nav_list").children(".submenu").css("height", navHeight);
 
-    //当文档窗口发生改变时 触发
-    $(window).resize(function(){
-	$("#main-container").height($(window).height()-76);
-	$("#iframe").height($(window).height()-140);
-	$(".sidebar").height($(window).height()-99);
-	var thisHeight = $("#nav_list").height($(window).height()-185);
-//	$(".submenu").height($(thisHeight).height()-160);
-	$("#nav_list").children(".submenu").css("height",thisHeight);
-  });
-    $(document).on("click",'.iframeurl',function(){
+    $(window).resize(function () {
+        var windowHeight = $(window).height();
+        $("#main-container").height(windowHeight - 76);
+        $("#iframe").height(windowHeight - 140);
+        $(".sidebar").height(windowHeight - 99);
+        var navHeight = $("#nav_list").height(windowHeight - 185);
+        $("#nav_list").children(".submenu").css("height", navHeight);
+    });
+    $(document).on("click", '.iframeurl', function () {
         var cid = $(this).attr("name");
-		var cname = $(this).attr("title");
-		$(".submenu,#nav_list").find(".active").removeClass("active");
-		$(this).parent().addClass("active");
-        $("#iframe").attr("src",cid).ready();
-		$("#Bcrumbs").attr("href",cid).ready();
-		$(".Current_page a").attr('href',cid).ready();	
-		$(".Current_page").html(cname).ready();	
-		$("#parentIframe").html(""). css("display","none").ready();						
-      });  
-	  	
-});
- 
-/*********************点击事件*********************/
-$( document).ready(function(){
-	  $('#nav_list').find('li.home').click(function(){
-	  $('#nav_list').find('li.home').removeClass('active');
-$(this).addClass('active');
-  });	
+        var cname = $(this).attr("title");
+        $(".submenu,#nav_list").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
+        $("#iframe").attr("src", cid).ready();
+        $("#Bcrumbs").attr("href", cid).ready();
+        $(".Current_page a").attr('href', cid).ready();
+        $(".Current_page").html(cname).ready();
+        $("#parentIframe").html("").css("display", "none").ready();
+    });
+    $('#nav_list').find('li.home').click(function () {
+        $('#nav_list').find('li.home').removeClass('active');
+        $(this).addClass('active');
+    });
 });
 
 
